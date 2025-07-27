20884e19f5030db701d1de8ff6daa9818e34d032
wyuxiaoyue <wyuxiaoyue@outlook.com>
2025-07-27 09:40:43 +0800

    [更新] 升级LibreTV版本

--- templates/config.js
+++ templates/config.js
@@ -23,6 +23,16 @@ const SITE_CONFIG = {
 // API站点配置
 const API_SITES = {{ sites }};
 
+// 定义合并方法
+function extendAPISites(newSites) {
+    Object.assign(API_SITES, newSites);
+}
+
+// 暴露到全局
+window.API_SITES = API_SITES;
+window.extendAPISites = extendAPISites;
+
+
 // 添加聚合搜索的配置选项
 const AGGREGATED_SEARCH_CONFIG = {
     enabled: true,             // 是否启用聚合搜索
